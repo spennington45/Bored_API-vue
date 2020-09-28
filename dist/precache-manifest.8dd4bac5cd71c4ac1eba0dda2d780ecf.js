5d500afca7a6cbf0e3433c5442abcecd90cbf9d8
Steven Pennington <spennington45@gmail.com>
2020-09-28 16:37:47 -0400

    dist

---
+++ dist/precache-manifest.8dd4bac5cd71c4ac1eba0dda2d780ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f53f4b1be1748848993",
+    "url": "/css/app.d74add27.css"
+  },
+  {
+    "revision": "ec57f2bbf109b28ddf9dd503d60df292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f53f4b1be1748848993",
+    "url": "/js/app.49c5cc1e.js"
+  },
+  {
+    "revision": "596c25bf02693e0f9079",
+    "url": "/js/chunk-vendors.f6dfd5c1.js"
+  },
+  {
+    "revision": "22c47d7fdf33d9439ef305267952f21d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
